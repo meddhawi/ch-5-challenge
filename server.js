1506777ca1b5018321c1fe66ff1cc5d8f9e7ef46
meddhawi <meddhawi@gmail.com>
2021-10-28 17:11:55 +0700

    installed necessary installation

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+// Setting template engine EJS
+app.set('view engine', 'ejs')
+
+
+// app.use(express.urlencoded({extended: false}));
+// app.use(express.static('public'));
+
+// app.use(express.json());
+
+
+app.get('/', function(req, res){
+    res.render('user')
+})
+
+app.get('/game', function(req, res){
+    res.render('game')
+})
+
+app.listen(port, () => {
+    console.log(`Go to http://localhost:${port}`)
+})
